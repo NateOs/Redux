7290c3c35c17ec16590e52451b5cec0ef9ba742b
nateos <sodjanathan@gmail.com>
2021-06-16 07:08:25 +0000

    git init

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from "react";
+// components
+import Navbar from "./components/Navbar";
+import CartContainer from "./components/CartContainer";
+// items
+import cartItems from "./cart-items";
+// redux stuff
+
+//store
+import { createStore } from "redux";
+
+//initial store
+const initialStore = {
+  count: 0,
+};
+
+//reducer
+const reducer = (state, action) => {
+  console.log({ state, action });
+  return state;
+};
+
+const store = createStore(reducer, initialStore);
+
+function App() {
+  // cart setup
+
+  return (
+    <main>
+      <Navbar />
+      <CartContainer cart={cartItems} />
+    </main>
+  );
+}
+
+export default App;
